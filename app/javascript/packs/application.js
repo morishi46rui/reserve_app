b9be34160b5da76a4dfc7d005e7b374187e342ca
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2022-12-17 22:10:22 +0900

    マイページのビューの編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).ready(() => {
+  $('.toggle').on('click', (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    $('#' + e.target.getAttribute('aria-controls')).toggleClass('is-hidden');
+  })
+})
